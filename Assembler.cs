04deff24ad98776ac472ad38abdbae2eae6e1f59
Tegan <tegan.straley@ucdenver.edu>
2016-03-01 10:44:27 -0700

    update Assembler.cs , better layout

--- Assembler.cs
+++ Assembler.cs
@@ -12,80 +12,44 @@ using System.Text;
 using System.Text.RegularExperssions;
 using System.Collections.Generic;
 
-namespace assembler
+namespace Assembler
 {
-     class SymbolTable
-     {
-          public static Dictionary<string, int> symbolTable = new Dictionary<string, int>()
-		{
-			{"SP", 0},
-			{"LCL", 1},
-			{"ARG", 2},
-			{"THIS", 3},
-			{"THAT", 4},
-			{"WRITE", 18},
-			{"R0", 0},
-			{"R1", 1},
-			{"R2", 2},
-			{"R3", 3},
-			{"R4", 4},
-			{"R5", 5},
-			{"R6", 6},
-			{"R7", 7},
-			{"R8", 8},
-			{"R9", 9},
-			{"R10", 10},
-			{"R11", 11},
-			{"R12", 12},
-			{"R13", 13},
-			{"R14", 14},
-			{"R15", 15},
-			{"SCREEN" 0x4000},
-			{"KBD" 0x6000}
-		};
-
-     }//end of SymbolTable
-
      class Assembler
      {
+     	//GLOBAL VARIABLES
+     	int lineNo;
+     	string line;
+     	string symbol;
+     	string dest;
+     	string comp;
+     	string jump;
+     	commandType = Parser.CommandType();
+     	
+     	
           public static void Main(string[] args)
           {
-               var inputFileList = new List<String>();
-               var outputFileList = new List<String>();
-
-               openFiles(inputFileList);
+		string asmFileName;
+		Console.WriteLine("Enter in the .asm file you wish to convert to .hack : ");
+		asmFileName = Console.ReadLine();
+			
+		if(File.Exists(asmFileName)){
+                	//start reading line by line until EOF 
+                	string[] lines = System.IO.File.ReadAllLines(asmFileName);
+               	}//end of if
+		else{
+			Console.WriteLine("Sorry you entered an invalid file name!\n Program terminating...\n");
+		}//end of else
+		
+		
+		parseLine();
+		pass1();
+		parseLine();
+		pass2();
 
           }//end of main
 
-          public static List<String> openFiles(List<String> inputFileList)
-		{
-			string asmFileName;
-			Console.WriteLine("Enter in the .asm file you wish to convert to .hack : ");
-			asmFileName = Console.ReadLine();
-			
-			if(File.Exists(asmFileName)){
-                    //start reading line by line until EOF 
-                    string[] lines = System.IO.File.ReadAllLines(asmFileName);
-                    for (int i = 0; i < lines.Length; i++)
-                    {
-                         string noWhiteSpacesLine = lines[i].Replace(" ", "");
-                         Console.WriteLine(noWhiteSpacesLine); // will this save to file or just write it out onto screen? 
-                         
-                        List<string> fileContents = controlFile.text.StripComments();
-			foreach (string ln in lines) Debug.Log(ln); //don't know if this line is effective. 
-			//foreach steps through each line or element of array like a for loop
-                    }
-				    
-			  
-               }//end of if
-			else{
-				Console.WriteLine("Sorry you entered an invalid file name!\n Program terminating...\n");
-			}//end of else
-
-               return inputFileList;
-				
-		}//end of readFile
-		
+         
+/*		
 //*********** CLASS DEC/DEF *************
 public static class ExtensionsSystem //new class to take in 
 {
@@ -116,3 +80,4 @@ public static class ExtensionsSystem //new class to take in
 // File.WriteAllLines(t, l);
 // File.Delete(fileName);
 // File.Move(t, fileName);
+*/
