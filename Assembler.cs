860ccc419b64be9cdb977dc180f2d3789f8bdebc
Tegan <tegan.straley@ucdenver.edu>
2016-03-02 19:14:11 -0700

    still can't get any file to open

--- Assembler.cs
+++ Assembler.cs
@@ -1,3 +1,4 @@
+
 //TEGAN STRALEY & CATIE COOK
 //FILE: Assembler.cs
 //PROJECT: created for project 6 of NAND2Tetris course
@@ -9,127 +10,146 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExperssions;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-namespace Assembler
+namespace AssemblerLab
 {
-     class Assembler
-     {
-     	enum Parser_CommandType {Parser_NO_COMMAND=0, Parser_A_COMMAND, Parser_C_COMMAND, Parser_L_COMMAND };
-     	
-     	//GLOBAL VARIABLES
-     	int lineNo = 0; // should start out at zero?
-     	string line;
-     	string symbol;
-     	string dest;
-     	string comp;
-     	string jump;
-     	Parser_CommandType commandType;
-     	
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MAIN FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-          public static void Main(string[] args)
-          {
-		string asmFileName;
-		Console.WriteLine("Enter in the .asm file you wish to convert to .hack : ");
-		asmFileName = Console.ReadLine();
-			
-		if(File.Exists(asmFileName)){
-                	while ((line = asmFileName.ReadLine()) != null) { //line by line each loop through
-                	
-                		parseLine(); 
-                		pass1();
-                		
-                		lineNo++; 
-                	}
-                
-               	}//end of if
-		else{//incorrect file name
-			Console.WriteLine("Sorry you entered an invalid file name!\n Program terminating...\n");
-		}//end of else
-		
-		//surround this with a while like before ??? 
-		parseLine(); 
-                pass2(); //writes to the output file .hack      
-		
-          }//end of main
+    class Assembler
+    {
+        enum Parser_CommandType { Parser_NO_COMMAND = 0, Parser_A_COMMAND, Parser_C_COMMAND, Parser_L_COMMAND };
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PARSE FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void parseLines(){ //takes out white space & comments
-	//set most global variables to null 
-     	symbol = NULL;
-     	dest = NULL;
-     	comp = NULL;
-     	jump = NULL
-     	commandType = Parser_NO_COMMAND;
-     	bool keepGoing = true;
+        //GLOBAL VARIABLES
+        int lineNo = 0; // should start out at zero?
+        string line;
+        string symbol;
+        string dest;
+        string comp;
+        string jump;
+        Parser_CommandType commandType;
 
-     	//create char array to fill with 
-     	char[] parsedLine = new char[line.length];
-     	int j = 0; //line counter for parsedLine
-     
-     	//get rid of white space
-     	for (int i = 0; i < line.length && keepGoing == true; i++){
-     		
-     		if(line[i] == '/'){
-     			if(line[i+1] == '/'){
-     				//comment has been found! Don't copy any of the rest of the line
-     				keepGoing = false; //set keepGoing to false and will fall out of for loop
-     			}
-     		}
-     		else(line[i].isAlpha()){ //isAlpha in c#?
-     			parsedLine[j] = line[i];
-     		}
-     		
-     	}
-     	
-     	//get rid of comments
-     	
-	
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PARSE FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public void parseLine()
+        { //takes out white space & comments
+          //set most global variables to null 
+            symbol = null;
+            dest = null;
+            comp = null;
+            jump = null;
+
+
+            //commandType = Parser_NO_COMMAND;
+            bool keepGoing = true;
+            Regex regex = new Regex("^[a-zA-Z0-9]*$");
+
+            //create char array to fill with 
+            char[] parsedLine = new char[line.Length];
+            int j = 0; //line counter for parsedLine
+
+            //get rid of white space
+            for (int i = 0; i < line.Length && keepGoing == true; i++)
+            {
+
+                if (line[i] == '/')
+                {
+                    if (line[i + 1] == '/')
+                    {
+                        //comment has been found! Don't copy any of the rest of the line
+                        keepGoing = false; //set keepGoing to false and will fall out of for loop
+                    }
+                }
+                else if (regex.IsMatch(line[i].ToString()))
+                {
+                    parsedLine[j] = line[i];
+                }
+
+            }
+
+            //get rid of comments
+
+
+        }
+
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MAIN FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public static void Main(string[] args)
+        {
+            Assembler assembler = new Assembler();
+            string asmFileName;
+            Console.WriteLine("Enter in the .asm file you wish to convert to .hack : ");
+            asmFileName = Console.ReadLine();
+            string line;
+            
+
+            if (File.Exists(@"c:\" + asmFileName + ".txt"))
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(asmFileName + ".txt");
+                while ((line = file.ReadLine()) != null)
+                { //line by line each loop through
+                    Console.WriteLine("GOT INTO while ! \n");
+                    asmFileName = Console.ReadLine();
+                    assembler.parseLine();
+                    //pass1();
+
+                    assembler.lineNo++;
+                }
+
+            }//end of if
+            else {//incorrect file name
+                Console.WriteLine("Sorry you entered an invalid file name!\n Program terminating...\n");
+                asmFileName = Console.ReadLine(); // just here so VS window doesn't close as quick
+            }//end of else
+
+            //surround this with a while like before ??? 
+            //parseLine();
+            //pass2(); //writes to the output file .hack      
+
+        }//end of main
+
+
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PASS1 FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        void pass1()
+        { //takes out white space & comments
+
+            //purpose:  to add new (SYMBOLS) to the SymbolTable
+
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PASS2 FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        void pass2()
+        { //takes out white space & comments
+
+            //purpose to convert line (parsed line?) to binary and write each line to the outfile
+        }
+    }
 }
-   
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PASS1 FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void pass1(){ //takes out white space & comments
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END OF PASS2 FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+        /*		
+        //*********** CLASS DEC/DEF *************
+        public static class ExtensionsSystem //new class to take in 
+        {
+            List<string> StripComments(this string inputFileList) //takes in the input file
+            {
+                List<string> result = new List<string>(inputFileList.Split(new string[] {"\r", "\n"}, 
+                StringSplitOptions.RemoveEmptyEntries) 
+                ); //uses array to look through file
+            result = result
+                .Where(line => !(line.StartsWith("//") || line.StartsWith("#"))) // where there is a line comment
+                .ToList(); // not sure...
 
-	//purpose:  to add new (SYMBOLS) to the SymbolTable
-	
-}
+                return result; 
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PASS2 FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void pass2(){ //takes out white space & comments
+            }//end of StripComments definition
 
-     //purpose to convert line (parsed line?) to binary and write each line to the outfile
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END OF PASS2 FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-/*		
-//*********** CLASS DEC/DEF *************
-public static class ExtensionsSystem //new class to take in 
-{
-	List<string> StripComments(this string inputFileList) //takes in the input file
-	{
-		List<string> result = new List<string>(inputFileList.Split(new string[] {"\r", "\n"}, 
-		StringSplitOptions.RemoveEmptyEntries) 
-		); //uses array to look through file
-	result = result
-		.Where(line => !(line.StartsWith("//") || line.StartsWith("#"))) // where there is a line comment
-		.ToList(); // not sure...
-		
-		return result; 
-			
-	}//end of StripComments definition
-	
-}//end of class dec/def
-
-     }//end of Assembler
-}//end of namespace
-
-
-
-//**** Options? ****
-// Found this, we could write to a new file that exclused both white space and comments
-// var t= Path.GetTempFileName();
-// var l= File.ReadLines(fileName).Where(l => !l.StartsWith("//") || !l.StartsWith("#"));
-// File.WriteAllLines(t, l);
-// File.Delete(fileName);
-// File.Move(t, fileName);
-*/
+        }//end of class dec/def
+             }//end of Assembler
+        }//end of namespace
+        //**** Options? ****
+        // Found this, we could write to a new file that exclused both white space and comments
+        // var t= Path.GetTempFileName();
+        // var l= File.ReadLines(fileName).Where(l => !l.StartsWith("//") || !l.StartsWith("#"));
+        // File.WriteAllLines(t, l);
+        // File.Delete(fileName);
+        // File.Move(t, fileName);
+        */
